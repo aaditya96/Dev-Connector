8f98b18d4d50acd1f33c284e67ec2a85709317ad
Aaditya Pandilwar <aadityap.pandilwar@gmail.com>
2021-01-16 23:34:42 +0530

    add final changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./db/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,10 +10,18 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/welcome', (req, res) => res.send('Welcome to the dev connector!'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//serve static webapp in production mode
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`App started on port: ${PORT}`));
